e3e85f3d4739abfba642d972d589410f21fba510
Onur Yerlikaya <onur.yerlikaya1@gmail.com>
2020-05-03 16:17:14 +0300

    photos added to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,38 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import "./index.css"
 
-const IndexPage = () => (
-  <div>
-    <h1>Merhaba</h1>
-  </div>
-)
+const IndexPage = ({ data }) => {
+  console.log(data)
+  return (
+    <div>
+      <h1>Merhaba</h1>
+      <div className="photos">
+        {data.allFile.edges.map(edge => {
+          return (
+            <Img key={edge.node.id} fluid={edge.node.childImageSharp.fluid} />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export const pageQuery = graphql`
+  {
+    allFile(filter: { absolutePath: { regex: "//photos//" } }) {
+      edges {
+        node {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
